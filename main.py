e36936018e65a8b9f2f24adace6f2eacf14c8df7
Chris Heise <crheise@icloud.com>
2023-09-19 12:06:32 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# Programmer: Chris Heise (crheise@icloud.com)
+# Course: BSSD 4350 Agile Methodoligies
+# Instructor: Jonathan Lee
+# Program: Together AI POC
+# Purpose: Build a POC using Together AI and Langchain for inclusivity app.
+# File: main.py
